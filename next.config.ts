a5e43a52a9bf426be6e6698a517a77051a33ea85
toddlasley <todd.r.lasley@gmail.com>
2025-01-04 15:27:22 -0800

    trailingSlash: true

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  trailingSlash: true
 };
 
 export default nextConfig;
