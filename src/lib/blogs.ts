368b35712d12bfe0ff8e30aed004e8f08d1bdd09
toddlasley <todd.r.lasley@gmail.com>
2025-06-10 14:05:13 -0700

    date fix

--- src/lib/blogs.ts
+++ src/lib/blogs.ts
@@ -9,12 +9,11 @@ export type Blog = {
 const blogs = new Map<string, Blog>();
 
 export const BLOGS = [
-  { id: '1', title: 'It all started at Christmas', date: '2025-12-25', markdown: 'christmas.md' }
+  { id: '1', title: 'It all started at Christmas', date: '2024-12-25', markdown: 'christmas.md' }
 ];
 
 export default function useBlogs() {
   if (!blogs.size) {
-    console.log('building blogs');
     for (const b of BLOGS) {
       blogs.set(b.id, b);
     }
